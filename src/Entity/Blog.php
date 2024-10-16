32ab910dd4aac8d92f3de0bb048a53d916ca9fd2
loganesov <justmadethisworldin@gmail.com>
2024-10-16 18:51:23 +0300

    lesson 5

---
+++ src/Entity/Blog.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BlogRepository::class)]
+class Blog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $text = null;
+  
+    #[ORM\ManyToOne(targetEntity: Category::class)]
+    #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id')]
+    private Category|null $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+  
+    public function getDescription(): ?string
+    {
+      return $this->description;
+    }
+    
+    public function setDescription(?string $description): static
+    {
+      $this->description = $description;
+      
+      return $this;
+    }
+  
+  public function getCategory(): ?Category
+  {
+    return $this->category;
+  }
+  
+  public function setCategory(?Category $category): static
+  {
+    $this->category = $category;
+    
+    return $this;
+  }
+}
